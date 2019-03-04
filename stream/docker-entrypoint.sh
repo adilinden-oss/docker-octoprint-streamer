368ccacd31f792e5c04b1a2edda8def73f3bf09a
Adi Linden <adi@adis.ca>
2019-03-03 22:41:16 -0600

    Container to wrapping adilinden/ffmpeg for easier use with environement variables.

---
+++ stream/docker-entrypoint.sh
@@ -0,0 +1,23 @@
+#!/bin/ash
+
+# Allow CTR-C to exit while loop
+exit_func() {
+        echo "SIGTERM detected"            
+        exit 1
+}
+trap exit_func SIGTERM SIGINT
+
+# If we are called with "stream" then run ffmpeg with 
+# the command line options passed in $FFMPEG_CMD. We use
+# "eval" here to be able to nest environment variables
+# inside environment variables for a clean yet flexible
+# docker-compose file.
+
+if [ "$1" = "stream" ]; then
+    ffmpeg_cmd=$(eval echo "$FFMPEG_CMD")
+    exec ffmpeg $ffmpeg_cmd
+fi
+
+exec "$@"
+
+# End
